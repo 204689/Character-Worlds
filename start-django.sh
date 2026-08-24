0cd9c6ea52807f7253bc7acd49e33b7833e80984
ArmandoPaPa <armands.blumbergs@gmail.com>
2026-08-24 17:28:14 +0300

    Fix Railway startup configuration

--- start-django.sh
+++ start-django.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-poetry run python manage.py collectstatic --no-input
+poetry run python manage.py collectstatic --no-input --ignore=input.css
 poetry run python manage.py migrate
 
 if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn djangocourse.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+    poetry run gunicorn djangocourse.wsgi --bind "0.0.0.0:${PORT:-8000}" \
+        --workers "${GUNICORN_WORKERS:-2}" --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
